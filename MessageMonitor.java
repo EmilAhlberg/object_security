173df9aa2100d62cac53c57f0dececf766d9832e
Ville <dat15vak@student.lu.se>
2018-09-19 13:03:31 +0200

    Formating

--- MessageMonitor.java
+++ MessageMonitor.java
@@ -2,19 +2,18 @@ import java.util.LinkedList;
 
 public class MessageMonitor{
 
-  LinkedList<String> messages = new LinkedList<String>();
+    LinkedList<String> messages = new LinkedList<String>();
 
-  public synchronized void NewMessage(String message){
-    messages.add(message);
-    notifyAll();
-  }
-
-  public synchronized String GetLatestMessage(){
-    while(messages.isEmpty()){
-      wait();
+    public synchronized void NewMessage(String message){
+        messages.add(message);
+        notifyAll();
     }
-    return messages.getFirst();
-  }
 
+    public synchronized String GetLatestMessage(){
+        while(messages.isEmpty()){
+            wait();
+        }
+        return messages.getFirst();
+    }
 
 }
