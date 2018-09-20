ac54af52e87571f0c2a4525eef10ee3046fe1be5
Ville <dat15vak@student.lu.se>
2018-09-20 09:43:33 +0200

    Added users A and B

--- Certificates/createClientCert.sh
+++ Certificates/createClientCert.sh
@@ -1,32 +1,30 @@
 #!/bin/sh
 echo "\nCreating new certificate\n"
-read -p "First name: " name
-read -p "Last name: " lname
-read -p "SSN-number: " ssn
+read -p "Name: " name
 read -s -p "Password: " pass
 echo "\n"
-mkdir $ssn
+mkdir $name
 
-## import CA to client truststore
-echo "ja" | keytool -import -file hospitalCA.crt -alias hospitalca -keystore clienttruststore -storepass $pass
+## import CA to truststore
+echo "ja" | keytool -import -file CA.crt -alias ca -keystore truststore -storepass $pass
 
-## generate keypair for client
-keytool -genkeypair -keystore clientkeystore -alias client -keypass $pass -storepass $pass -dname "CN=$ssn"
+## generate keypair
+keytool -genkeypair -keystore keystore -alias $name -keypass $pass -storepass $pass -dname "CN=$ssn"
 
 ## create CSR
-keytool -certreq -file certreq.crt -keystore clientkeystore -alias client -storepass $pass
+keytool -certreq -file certreq.crt -keystore keystore -alias $name -storepass $pass
 
 ## sign CSR using CA
-openssl x509 -req -in certreq.crt -CA hospitalCA.crt -CAkey private.key -CAcreateserial -out client.crt -passin pass:password
+openssl x509 -req -in certreq.crt -CA CA.crt -CAkey private.key -CAcreateserial -out $name.crt -passin pass:password
 
-## import CA to client keystore
-echo "ja" | keytool -import -file hospitalCA.crt -alias hospitalca -keystore clientkeystore -storepass $pass
+## import CA to keystore
+echo "ja" | keytool -import -file CA.crt -alias ca -keystore keystore -storepass $pass
 
-## import signed client cert to client keystore
-keytool -import -file client.crt -alias client -keystore clientkeystore -storepass $pass
+## import signed cert to keystore
+keytool -import -file $name.crt -alias $name -keystore keystore -storepass $pass
 
 rm certreq.crt
-rm client.crt
-mv clientkeystore $ssn
-mv clienttruststore $ssn
+rm $name.crt
+mv keystore $name
+mv truststore $name
 echo "\nNew user certificate created"
