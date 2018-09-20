cb439cb023c9bed2d6202d379912941f6fa5b858
Ville <dat15vak@student.lu.se>
2018-09-20 12:45:43 +0200

    Added a message and state skeleton

--- MessageMonitor.java
+++ MessageMonitor.java
@@ -5,44 +5,58 @@ import java.net.InetAddress;
 
 public class MessageMonitor{
 
- private  LinkedList<String> messages = new LinkedList<String>();
+    private  LinkedList<String> messages = new LinkedList<String>();
+    private int currentState;
+    private static final int HANDSHAKE = 0;
+    private static final int DATA_TRANSFER = 1;
 
- public MessageMonitor() {
-  messages.add("första meddelandet!");
- }
+    public MessageMonitor() {
+        messages.add("första meddelandet!");
+        currentState = HANDSHAKE;
+    }
 
- public synchronized void newMessage(DatagramPacket packet){
-  String msg = new String(packet.getData()).trim();
-  System.out.println("Recieved message: " + msg);
-  messages.add(msg);
-  notifyAll();
- }
+    public synchronized void newMessage(DatagramPacket packet){
+        String msg = new String(packet.getData()).trim();
+        System.out.println("Recieved message: " + msg);
+        messages.add(msg);
+        notifyAll();
+    }
 
- public synchronized void sendMessage(int port, DatagramSocket socket) throws Exception {
-  while (messages.isEmpty()) {
-    System.out.println("wait!");
-     wait();
-  }
-  messages.pop();
-  InetAddress IPAddress = InetAddress.getByName("localhost");
-  String msg = "hej din port är: " + port;
-  DatagramPacket p = new DatagramPacket(
-  msg.getBytes(), msg.getBytes().length, IPAddress, port);
-  socket.send(p);
+    public synchronized void sendMessage(int port, DatagramSocket socket) throws Exception {
+        while (messages.isEmpty()) {
+            System.out.println("wait!");
+            wait();
+        }
+        parseMessage();
+        // InetAddress IPAddress = InetAddress.getByName("localhost");
+        // String msg = "hej din port är: " + port;
+        // DatagramPacket p = new DatagramPacket(
+        // msg.getBytes(), msg.getBytes().length, IPAddress, port);
+        socket.send(p);
 
-}
+    }
 
-public synchronized String getLatestMessage(){
- try{
-  while(messages.isEmpty()){
-   wait();
-  }
-  return messages.getFirst();
- }
- catch(InterruptedException e) {
-  System.out.println(e.getMessage());
- }
- return null;
-}
+    private void parseMessage(){
+        byte[] message = messages.pop();
+        currentState = message[0];
+        switch(currentState){
+            case HANDSHAKE:
+            handleHandshake(message);
+            break;
+            case DATA_TRANSFER:
+            handleDataTransfer(message);
+            break;
+            default:
+            break;
+        }
+    }
+
+    private void handleHandshake(byte[] message){
+
+    }
+
+    private void handleDataTransfer(byte[] message){
+
+    }
 
 }
